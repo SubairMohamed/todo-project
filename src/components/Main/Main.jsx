09dc2f169cd951de2854e7ab784796f8bfd23eaa
SubairMohamed <subairmohamed44@gmail.com>
2022-06-29 15:18:16 +0300

    making-header-style-font-twl-index

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,14 @@
 const Main = () => {
     return (
         // Main
-        <section className="grow">
-            <header>
-                <div>
-                    <h1>Learn todo app </h1>
-                    <p>sava your performance</p>
+        <section className="grow font-RobotoSlab">
+            <header className="mx-4 my-3 flex items-center justify-between">
+                <div id="text-header">
+                    <h1 className="text-teal-400 text-xl font-bold capitalize tracking-wide">Learn todo app </h1>
+                    <p className="text-sm text-teal-400 font-thin capitalize tracking-wider">sava your performance</p>
+                </div>
+                <div className="bg-sky-800 p-1 rounded-full">
+                    <button className="text-sm text-teal-500">Add</button>
                 </div>
             </header>
             <main>main</main>
