4c40b6ea9d3c77968715f541adade9cdf197fa03
SubairMohamed <subairmohamed44@gmail.com>
2022-06-29 15:52:03 +0300

    making-style-and-install-heroicons

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,3 +1,5 @@
+import { PlusCircleIcon } from "@heroicons/react/solid";
+
 const Main = () => {
     return (
         // Main
@@ -7,8 +9,8 @@ const Main = () => {
                     <h1 className="text-teal-400 text-xl font-bold capitalize tracking-wide">Learn todo app </h1>
                     <p className="text-sm text-teal-400 font-thin capitalize tracking-wider">sava your performance</p>
                 </div>
-                <div className="bg-sky-800 p-1 rounded-full">
-                    <button className="text-sm text-teal-500">Add</button>
+                <div className="bg-purple-800 p-1 rounded-full w-7 h-7 flex justify-center items-center">
+                    <button className="text-sm text-white"><PlusCircleIcon className="h-7 w-7" /></button>
                 </div>
             </header>
             <main>main</main>
