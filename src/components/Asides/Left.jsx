92dccb67c151fc29f61bb4733431ea7bb697b1f8
SubairMohamed <subairmohamed44@gmail.com>
2022-07-01 16:53:27 +0300

    looking-with-left-logo

--- src/components/Asides/Left.jsx
+++ src/components/Asides/Left.jsx
@@ -2,8 +2,8 @@ const Left = () => {
     return (
         //    aside left 
         <aside id="left" className=" w-20 lg:w-28 flex-none shadow-xl shadow-sky-200 rounded-tr-lg bg-sky-600">
-            <div id="logo" className="my-4 mx-4">
-                <img src="/images/todo-logo-1.jpg" className="rounded-full shadow-md shadow-sky-300"></img>
+            <div id="logo" className="my-4 mx-6">
+                <img src="/images/todo-logo-1.jpg" className="rounded-full shadow-md shadow-sky-300 sm:w-12 md:w-16"></img>
             </div>
         </aside>
     );
