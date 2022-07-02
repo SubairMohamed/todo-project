351f6c7744fc4a4f5c8d8b28e2cf0584668fff40
subair44 <subairmohamed44@gmail.com>
2022-07-02 20:40:10 +0300

    making-responsive

--- src/components/Asides/Left.jsx
+++ src/components/Asides/Left.jsx
@@ -1,10 +1,17 @@
 const Left = () => {
     return (
         //    aside left 
-        <aside id="left" className=" w-20 lg:w-28 flex-none shadow-xl shadow-sky-200 rounded-tr-lg bg-sky-600">
-            <div id="logo" className="my-4 mx-6">
-                <img src="/images/todo-logo-1.jpg" className="rounded-full shadow-md shadow-sky-300 sm:w-12 md:w-16"></img>
+        <aside id="left" className="w-20 lg:w-28 xl:w-60 lex-none shadow-xl shadow-sky-200 rounded-tr-lg bg-sky-600">
+            <div id="wrap-logo" className="flex flex-row justify-center items-center">
+                <div id="logo" className="my-4 mx-3">
+                    <img src="/images/todo-logo-1.jpg" className="rounded-full shadow-md shadow-sky-300 w-12"></img>
+                </div>
+                <div id="text-header" className="hidden xl:block">
+                    <h1 className="text-teal-400 text-xl font-bold capitalize">Learn todo app </h1>
+                    <p className="text-sm text-teal-400 font-thin capitalize">sava your performance</p>
+                </div>
             </div>
+
         </aside>
     );
 }
--- src/components/Asides/Right.jsx
+++ src/components/Asides/Right.jsx
@@ -1,7 +1,7 @@
 const Right = () => {
     return (
         //    aside right
-        <aside id="right" className="sm:hidden md:block md:w-60 lg:w-96 rounded-tl-lg shadow-lg shadow-sky-200 bg-sky-600"></aside>
+        <aside id="right" className="hidden lg:block md:w-60 xl:w-80 rounded-tl-lg shadow-lg shadow-sky-200 bg-sky-600"></aside>
     );
 }
 
