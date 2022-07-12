5a7c2b001056748b0e5f198cb14bb739b09df00f
subair44 <subairmohamed44@gmail.com>
2022-07-12 14:42:05 +0300

    making-style-categiry-title-forms

--- src/components/Main/Content/Form/Form.jsx
+++ src/components/Main/Content/Form/Form.jsx
@@ -4,14 +4,14 @@ const Form = () => {
     return (
         <section id='form' className="card-white w-full my-3">
             <h1 className="text-2xl font-bold tracking-wide capitalize mb-3 text-teal-600 underline">add new task</h1>
-            <form className="grid grid-cols-2 gap-3">
+            <form className="grid grid-cols-1 md:grid-cols-2 gap-3">
                 <div className="flex flex-row items-start">
                     <label htmlFor='title' className="text-lg font-bold tracking-wide mx-1">Title:</label>
-                    <input type="text" name="text" id='title' className="px-2 text-black"></input>
+                    <input type="text" name="text" id='title' className="px-2 text-black rounded-xl focus:outline-none focus:bg-teal-200 focus:border focus:border-sky-900"></input>
                 </div>
                 <div>
-                    <label htmlFor='category'>Category:</label>
-                    <input type="text" name="text" id='category'></input>
+                    <label htmlFor='category' className="text-lg font-bold tracking-wide mx-1">Category:</label>
+                    <input type="text" name="text" id='category' className="px-2 text-black rounded-xl focus:outline-none focus:bg-teal-200 focus:border focus:border-sky-900"></input>
                 </div>
                 <div>
                     <label htmlFor='date'>Date:</label>
