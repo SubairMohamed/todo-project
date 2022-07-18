13c3f302cc71d2325882592b0517d6b410f08f59
SubairMohamed <subairmohamed44@gmail.com>
2022-07-18 22:23:24 +0300

    making-multistate-in-forms

--- src/components/Main/Content/Form/Form.jsx
+++ src/components/Main/Content/Form/Form.jsx
@@ -3,19 +3,33 @@ import "./Form.css";
 
 const Form = () => {
 
-    const [title, settitle] = useState = ("");
-    const [category, setcategory] = useState = ("");
-    const [date, setdate] = useState = ("");
-    const [Status, setstatus] = useState = ("");
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [date, setDate] = useState("");
+    const [status, setStatus] = useState("");
 
-    const titleChangeHandler = (e) => { }
-    const categoryChangeHandler = (e) => { }
-    const dateChangeHandler = (e) => { }
-    const statusChangeHandler = (e) => { }
+    const titleChangeHandler = (e) => {
+        setTitle(e.target.value);
+    };
+    const categoryChangeHandler = (e) => {
+        setCategory(e.target.value);
+    };
+    const dateChangeHandler = (e) => {
+        setDate(e.target.value);
+    };
+    const statusChangeHandler = (e) => {
+        setStatus(e.target.value);
+    };
 
-    const submitFormHandler = () => {
-        console.log("form is submited");
-    }
+    const submitFormHandler = (e) => {
+        e.preventDefault();
+        console.log({
+            title,
+            category,
+            date,
+            status,
+        });
+    };
 
     return (
         <section id='form' className="card-gray w-full my-3">
@@ -23,20 +37,26 @@ const Form = () => {
             <form onSubmit={submitFormHandler} className="grid grid-cols-1 md:grid-cols-2 gap-3">
                 <div className="input-group">
                     <label htmlFor='title' className="input-label">Title:</label>
-                    <input type="text" name="text" id='title' className="input-control"></input>
+                    <input type="text" name="text" id='title' className="input-control" value={title} onChange={titleChangeHandler}></input>
                 </div>
                 <div className="input-group">
-                    <label htmlFor='category' className="input-label">Category:</label>
-                    <input type="text" name="text" id='category' className="input-control"></input>
+                    <label htmlFor='category' className="input-label ">Category:</label>
+                    <select name="category" className="input-control" value={category} onChange={categoryChangeHandler}>
+                        <option defaultValue="" className="text-gray-500">Choose Category</option>
+                        <option value="going to trip">going to trip</option>
+                        <option value="going to travel">going to travel</option>
+                        <option value="going to studying">going to studying</option>
+                        <option value="going to work">going to work</option>
+                    </select>
                 </div>
                 <div className="input-group">
                     <label htmlFor='date' className="input-label">Date:</label>
-                    <input type="date" name="date" id='date' className="input-control"></input>
+                    <input type="date" name="date" id='date' className="input-control" value={date} onChange={dateChangeHandler}></input>
                 </div>
                 <div>
                     <label htmlFor='status' className="input-label">Status:</label>
-                    <select className="input-control">
-                        <option disabled value="">Choose Status</option>
+                    <select name="status" className="input-control" value={status} onChange={statusChangeHandler}>
+                        <option defaultValue="" className="text-gray-500">Choose Status</option>
                         <option value="In Progress">In Progress</option>
                         <option value="Completed">Completed</option>
                     </select>
