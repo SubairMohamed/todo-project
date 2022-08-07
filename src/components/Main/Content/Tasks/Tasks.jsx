9d245aabf8d3091211ef22803f0457cbdb43ea8a
SubairMohamed <subairmohamed44@gmail.com>
2022-08-07 21:17:11 +0300

    looking-for-style-tasks

--- src/components/Main/Content/Tasks/Tasks.jsx
+++ src/components/Main/Content/Tasks/Tasks.jsx
@@ -7,7 +7,7 @@ const Tasks = (props) => {
 
     return (
         //    Tasks 
-        < div id="tasks" className="bg-sky-900 rounded-lg flex flex-col items-center justify-center px-2 py-2" >
+        < div id="tasks" className="bg-sky-900 rounded-lg flex flex-col items-center justify-center shadow-lg shadow-sky-900 px-2 py-3 mb-3">
             <h1 className="text-teal-500 text-2xl font-bold capitalize underline pb-1 tracking-wider w-full max-w-lg">lists</h1>
 
             <div id='Lists' className='w-full flex flex-col items-center justify-center'>
